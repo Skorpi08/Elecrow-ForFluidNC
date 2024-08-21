1c0ccd72b89761fbf859f13e66f87e309688f37a
Skorpi08 <skorpik08@gmail.com>
2024-08-21 22:21:29 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
+#include "PCA9557.h"
 #include "main.h"
 
 LGFX tft;
-
+PCA9557 Out;
 extern void updateDisplay();
 extern "C" void fnc_putchar(uint8_t c);
 extern void fnc_poll();
@@ -13,13 +14,23 @@ void display_set_brightness(int brightness)
 
 void setup(void)
 {
+    Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL);
+    Out.reset();
+    Out.setMode(IO_OUTPUT);
+    Out.setState(IO0, IO_LOW);
+    Out.setState(IO1, IO_LOW);
+    delay(20);
+    Out.setState(IO0, IO_HIGH);
+    delay(100);
+    Out.setMode(IO1, IO_INPUT);
+
     FNCSerial.begin(115200);
     tft.begin();
     fnc_putchar('?');
 #ifdef USE_LVGL
     lvgl_begin();
 #endif
- updateDisplay();
+    updateDisplay();
 }
 
 void loop(void)
